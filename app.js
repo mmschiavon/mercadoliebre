ae4567bda1e11081c4c89d93303d959aefce88ea
Gerry <90663312+ArmonDirac@users.noreply.github.com>
2021-10-05 15:40:08 -0300

    Conectado a CSS

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 
-app.use ("/static",express.static("public"))
+app.use ("/static",express.static('./public'));
+
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "views/home.html"))
