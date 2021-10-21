78064eb61153d6ff37b58ddf4dc77c5d50a9bafb
mmschiavon <nfo@iacon.com.ar>
2021-10-21 19:15:11 -0300

    commit 9

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const path = require ('path');
+const path = require("path");
 
-app.use ("/static",express.static('./public'));
+app.use("/static", express.static("./public"));
 
-
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/home.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
-app.listen (3000, ()=> {
-    console.log( "el servidor inicio...");
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.listen(3000, () => {
+  console.log("el servidor inicio...");
 });
