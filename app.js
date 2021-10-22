b3c24e263cbb2b80b6d071878d16fbe38e7de18e
mmschiavon <nfo@iacon.com.ar>
2021-10-21 21:11:54 -0300

    commit 10

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ app.use("/static", express.static("./public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/login.html"));
-});
+
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
 
 app.listen(3000, () => {
