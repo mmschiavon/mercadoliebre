3e23ea031c6a0e3cd40b5cb7b3287874919870f1
mmschiavon <nfo@iacon.com.ar>
2021-10-21 22:16:51 -0300

    primer commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use("/static", express.static("./public"));
 
